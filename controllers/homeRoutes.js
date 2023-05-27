93571018ea4514b201bde87e504cbe3982a0e9ca
ortiz MJ <ortizzzj99@gmail.com>
2023-05-27 14:35:02 -0700

    dont merge yet

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,6 +3,8 @@ const router = require('express').Router();
 
 router.get('/', async (req, res) => {
   try {
+
+
     res.render('homepage');
   } catch (err) {
     res.status(500).json(err);
@@ -19,3 +21,8 @@ router.get('/login', (req, res) => {
 });
 
 module.exports = router;
+
+
+/*
+router for all categories 
+*/
