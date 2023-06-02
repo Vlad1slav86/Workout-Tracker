58405985edf35c1ebe51b9811236080acf08a333
Joel Estrada <joeljazzhands@Joels-MBP.attlocal.net>
2023-06-01 20:58:43 -0700

    made it so you dont have to sign in to view the app

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -20,7 +20,7 @@ router.get('/', async (req, res) => {
 
     res.render('homepage', {
       posts,
-      logged_in: req.session.logged_in
+      // logged_in: req.session.logged_in
     });
 
   } catch (err) {
