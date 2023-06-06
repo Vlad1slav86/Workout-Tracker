e1eb87f027f600dbd19d746323a11591e7014d08
Joel Estrada <joeljazzhands@Joels-MBP.attlocal.net>
2023-06-05 19:28:47 -0700

    added changes to the profile styling

--- public/js/myprofile.js
+++ public/js/myprofile.js
@@ -23,13 +23,8 @@ const input = document.querySelector('.usr');
 const inputBtn = document.querySelector('.usrbtn');
 const form = document.querySelector('.profileform');
 const h3 = document.querySelector('.usrdata');
-const usr = document.querySelector('.usrdata2');
-const diet = document.querySelector('.usrdata3');
-const stats = document.querySelector('.usrdata4');
 const rememberMe = document.querySelector('.rememberme');
-const input1 = document.querySelector('#input1');
-const input2 = document.querySelector('#input2');
-const input3 = document.querySelector('#input3');
+
 
 
 form.addEventListener('submit', function (event){
@@ -52,23 +47,6 @@ function DisplayCheck(){
     h3.textContent = `${names}`;
     input.style.display = 'none';
     inputBtn.style.display = 'none';
-
-  } if (localStorage.getItem('Workout')){
-    let namer = localStorage.getItem('Workout');
-    usr.textContent = `${namer}`;
-    input1.style.display = 'none';
-
-  } if (localStorage.getItem('Diet')){
-    let namen = localStorage.getItem('Diet');
-    diet.textContent = `${namen}`;
-    input2.style.display = 'none';
-
-  } if (localStorage.getItem('Status')){
-    let naming = localStorage.getItem('Status');
-    stats.textContent = `${naming}`;
-    input3.style.display = 'none';
-
-  } 
-
+  }
 }
 document.body.onload = DisplayCheck;
