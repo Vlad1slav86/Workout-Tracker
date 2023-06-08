c8348951947090ac0869c914438bcef58bc266e5
Joel Estrada <joeljazzhands@Joels-MBP.attlocal.net>
2023-06-07 21:02:28 -0700

    commiting

--- public/js/myprofile.js
+++ public/js/myprofile.js
@@ -5,37 +5,36 @@ const newFormHandler = async (event) => {
   const photos = document.querySelector('.userimages').files;
   const user_id = document.querySelector('.img').value;
   console.log(user_id);
-  if (about){
-    console.log(photos);
-    console.log(about);
-    const response = await (`/api/users/${user_id}`, {
-      method: 'PUT',
-      body: JSON.stringify({about}),
-      headers: {
-        'Content-Type': 'application/json'
-      },
-    });
-    console.log(response);
-    if(response.ok && !photos.length){
-      document.location.replace('/profile');
-    } else if(photos.length) {
-      for (let i = 0; i < photos.length; i++) {
-        let formData = new FormData();
-        formData.append('photo', photos[i]);
-        formData.append('id', user_id);
-        fetch('/api/picture', {
-          method: 'POST',
-          body: formData,
-        }).then(() => {
-          console.log('photo uploaded');
-          document.location.reload();
-        });
-      }
-    }
-    else {
-      alert(response.statusText);
+  console.log(photos);
+  console.log(about);
+  const response = await fetch(`/api/users/${user_id}`, {
+    method: 'PUT',
+    body: JSON.stringify({about}),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  });
+  console.log(response);
+  if(response.ok && !photos.length){
+    document.location.replace('/myprofile');
+  } else if(photos.length) {
+    for (let i = 0; i < photos.length; i++) {
+      let formData = new FormData();
+      formData.append('photo', photos[i]);
+      formData.append('id', user_id);
+      fetch('/api/picture', {
+        method: 'POST',
+        body: formData,
+      }).then(() => {
+        console.log('photo uploaded');
+        document.location.reload();
+      });
     }
   }
+  else {
+    alert(response.statusText);
+  }
+  
 };
 
 
